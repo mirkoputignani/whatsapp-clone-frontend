3cc4429721453fba39d58e682d556f3df327d9b9
Mirko Putignani <dev.mputignani@gmail.com>
2020-09-09 10:45:51 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAMqipERX0xfqBQ4nmdX-HFHmISfxm7FL0",
+    authDomain: "whatsapp-mern-cf241.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-cf241.firebaseio.com",
+    projectId: "whatsapp-mern-cf241",
+    storageBucket: "whatsapp-mern-cf241.appspot.com",
+    messagingSenderId: "155944293569",
+    appId: "1:155944293569:web:3f209f35e3cbea99a67040",
+    measurementId: "G-R4Q3ECZDEN"
+};
